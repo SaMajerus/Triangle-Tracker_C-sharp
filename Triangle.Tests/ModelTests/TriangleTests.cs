c3feb74c2efd08d28393f293f786fd1e5096b450
Samuel Majerus <ladolego@gmail.com>
2022-09-20 09:02:42 -0700

    Renamed file with MSTest methods to be more consistent naming-wise
    Co-authored-by: Harold Mesa <harold.mesa@icloud.com>

--- Triangle.Tests/ModelTests/TriangleTests.cs
+++ Triangle.Tests/ModelTests/TriangleTests.cs
@@ -1,36 +1,32 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Calendar;
+using Triangle;
 
-namespace Calendar.Tests
+namespace Triangle.Tests
 {
   [TestClass]
-  public class LeapYearTests
-  {
+  public class TrackerTests {
+
     [TestMethod]
-    public void IsLeapYear_NumberDivisibleByFour_True()
-    {
+    public void IsLeapYear_NumberDivisibleByFour_True(){
       LeapYear testLeapYear = new LeapYear();
       Assert.AreEqual(true, testLeapYear.IsLeapYear(2012));
-      //Assert.AreEqual(false, testLeapYear.IsLeapYear(2011));  //Added before the Second test was -- it passed.
+      //Assert.AreEqual(false, testLeapYear.IsLeapYear(2011)); 
     }
     
     [TestMethod]
-    public void IsLeapYear_NumberNotDivisibleByFour_False()
-    {
+    public void IsLeapYear_NumberNotDivisibleByFour_False(){
       LeapYear testLeapYear = new LeapYear();
       Assert.AreEqual(false, testLeapYear.IsLeapYear(1999));
     }
 
     [TestMethod]
-    public void IsLeapYear_MultiplesOfOneHundred_False()
-    {
+    public void IsLeapYear_MultiplesOfOneHundred_False(){
       LeapYear testLeapYear = new LeapYear();
       Assert.AreEqual(false, testLeapYear.IsLeapYear(1900));
     }
 
     [TestMethod]
-    public void IsLeapYear_MultiplesOfFourHundred_True()
-    {
+    public void IsLeapYear_MultiplesOfFourHundred_True(){
       LeapYear testLeapYear = new LeapYear();
       Assert.AreEqual(true, testLeapYear.IsLeapYear(2000));
     }
